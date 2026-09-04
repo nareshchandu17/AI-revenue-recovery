0c38b93c97dd8488296ca4d2867e425607e19fd4
nareshchandu17 <nareshchandu27@gmail.com>
2026-09-04 11:52:56 +0530

    Fix turbopack root workspace warning

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  experimental: {
+    turbopack: {
+      root: './'
+    }
+  }
 };
 
 export default nextConfig;
